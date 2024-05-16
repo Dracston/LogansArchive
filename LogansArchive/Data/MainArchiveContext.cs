ea070a5fafea5b65eb20507a793728ada1ea9bdd
Logan Jones <lpj11101@gmail.com>
2024-05-16 12:07:47 -0700

    Added Db Context and updated Json

---
+++ LogansArchive/Data/MainArchiveContext.cs
@@ -0,0 +1,19 @@
+﻿using LogansArchive.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace LogansArchive.Data
+{
+    public class MainArchiveContext : DbContext
+    {
+        public MainArchiveContext(DbContextOptions<MainArchiveContext> options) : base(options) {
+        }
+
+        public DbSet<Director> Directors {get; set;}
+        public DbSet<Game> Games {get; set;}
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Show> Shows { get; set; }
+        public DbSet<Studio> Studios { get; set; }
+
+    }
+}
